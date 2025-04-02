6b49d4b68c44ab179b82596ad14f717da1a346d3
souf989 <soufiane.989@gmail.com>
2025-04-02 13:19:57 +0000

    infra si up

--- project/infrastructure/terraform/main.tf
+++ project/infrastructure/terraform/main.tf
@@ -1,5 +1,5 @@
 locals {
-  component_name = "<changer-le-nom>-${random_string.random.result}"
+  component_name = "proverb-${random_string.random.result}"
 }
 
 resource "random_string" "random" {
@@ -11,11 +11,13 @@ resource "random_string" "random" {
 #---------------------------------------------- S3
 
 #TODO : Créer un Bucket S3 pour héberger le front en mode site statique
-# ...
+resource "aws_s3_bucket" "front_bucket" {
+  bucket = "s3-${local.component_name}" # nom unique
+}
 
 #TODO : Rattacher au bucket s3
 resource "aws_s3_bucket_website_configuration" "front_bucket_website" {
-  bucket = # ...
+  bucket = aws_s3_bucket.front_bucket.id
 
   index_document {
     suffix = "index.html"
@@ -25,7 +27,7 @@ resource "aws_s3_bucket_website_configuration" "front_bucket_website" {
 
 #TODO : Rattacher au bucket s3
 resource "aws_s3_bucket_public_access_block" "front_bucket_block" {
-  bucket = # ...
+  bucket = aws_s3_bucket.front_bucket.id
 
   block_public_acls       = false
   block_public_policy     = false
@@ -35,7 +37,7 @@ resource "aws_s3_bucket_public_access_block" "front_bucket_block" {
 
 #TODO : Rattacher au bucket s3
 resource "aws_s3_bucket_policy" "front_bucket_policy" {
-  bucket = # ...
+  bucket = aws_s3_bucket.front_bucket.id
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
@@ -43,42 +45,49 @@ resource "aws_s3_bucket_policy" "front_bucket_policy" {
       Effect    = "Allow",
       Principal = "*",
       Action    = "s3:*",
-      Resource  = "${aws_s3_bucket#...}/*"
+      Resource  = "${aws_s3_bucket.front_bucket.arn}/*"
     }]
   })
 }
 
 # TODO:  Upload du fichier index.html dans le bucket S3
 resource "aws_s3_object" "front_index" {
-  bucket       = # ...
-  key          = # ...
+  bucket       = aws_s3_bucket.front_bucket.id
+  key          = "index.html"
   source       = "../../build/front_extracted/index.html"
-  content_type = # ...
+  content_type = "text/html"
   etag         = filemd5("../../build/front_extracted/index.html")
 }
 
 # TODO:  Upload du fichier script.js dans le bucket S3
 resource "aws_s3_object" "script" {
-  bucket       = # ...
-  key          = # ...
+  bucket       = aws_s3_bucket.front_bucket.id
+  key          = "script.js"
   source       = "../../build/front_extracted/script.js"
-  content_type = # ...
+  content_type = "application/javascript"
   etag         = filemd5("../../build/front_extracted/script.js")
 }
 
 # TODO:  Upload du fichier style.css dans le bucket S3
 resource "aws_s3_object" "style" {
-  bucket       = # ...
-  key          = # ...
+  bucket       = aws_s3_bucket.front_bucket.id
+  key          = "style.css"
   source       = "../../build/front_extracted/style.css"
-  content_type = # ...
+  content_type = "text/css"
   etag         = filemd5("../../build/front_extracted/style.css")
 }
 
 #---------------------------------------------- Lambda
 
 # TODO:  créer une lambda
-# ...
+resource "aws_lambda_function" "proverb_lambda" {
+  function_name    = "lambda-${local.component_name}"
+  role             = aws_iam_role.lambda_exec.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.8"
+  filename         = "../../build/backend-${var.version_backend}.zip"
+  source_code_hash = filebase64sha256("../../build/backend-${var.version_backend}.zip")
+}
 
 # Rôle IAM pour la fonction Lambda
 resource "aws_iam_role" "lambda_exec" {
@@ -107,5 +116,10 @@ resource "aws_iam_role_policy_attachment" "lambda_policy" {
 
 # TODO: Appeler le module api gateway
 # ...
-
+module "api_gateway" {
+  source                     = "./module"
+  api_gateway_name           = "apitgwy-${local.component_name}"
+  lambda_function_invoke_arn = aws_lambda_function.proverb_lambda.invoke_arn
+  lambda_function_name       = aws_lambda_function.proverb_lambda.function_name
+}
 
